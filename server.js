d0d4ea23897f5d6e30f93e7b1c7739bd5db6db26
chromeheartz <barnesquiat@gmail.com>
2022-08-24 22:40:38 +0900

    install bootstrap

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ app.listen(7777, function(){
   console.log('listening on 7777')
 });
 
-app.get('/beauty', function(req, res){
-  res.send('뷰티페이지에욤')
-});
-
-// /하나만 남겨주면 홈임.
 app.get('/', function(req, res){
-  // sendFile(보낼파일경로)를 쓰면 파일을 브라우저에 보낼수있다
   res.sendFile(__dirname + '/index.html')
 });
+
+app.get('/write.html', function(req,res){
+  res.sendFile(__dirname + '/write.html')
+})
